dc7bb804193b27bcb3f2b53da6fb4cacf230d7c0
Ambar Naz <ambarnaz11@gmail.com>
2025-02-11 22:52:58 +0500

    update

--- js/script.js
+++ js/script.js
@@ -142,3 +142,81 @@ mybutton.addEventListener("click",function(){
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
+
+
+function sendEmail(event) {
+  event.preventDefault(); // Prevent the form from submitting
+
+  let params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  console.log("Sending email with parameters:", params);
+
+  emailjs.send("service_fr8tjsp", "template_rv1s5nu", params)
+    .then(function(res) {
+      console.log("Email sent successfully:", res);
+      Swal.fire({
+        title: "Email Sent Successfully!",
+        text: "Thank you for contacting us.",
+        icon: "success"
+      });
+      document.getElementById("contact-form").reset();
+    })
+    .catch(function(err) {
+      console.error("Failed to send email:", err);
+      Swal.fire("Error", "Failed to send email. Check the console.", "error");
+    });
+}
+
+// Attach event listener to form
+document.getElementById("contact-form").addEventListener("submit", sendEmail);
+
+
+
+
+// pre loader
+
+$(window).on("load", function() {
+  $(".loader-wrapper").fadeOut(200);
+});
+
+setTimeout(function() {
+  if ($(".loader-wrapper").is(":visible")) {
+    $(".loader-wrapper").fadeOut(200);  // Fallback in case the load event doesn't trigger
+  }
+}, 5000);  // Force fade out after 5 seconds
+
+// autotype
+const typed = new Typed('.multiple-text',{
+  strings:['BSCS Student at IQRA University ','Frontend developer'],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+});
+
+// counter effect
+$(document).ready(function() {
+  $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+  });
+});
+
+
+locoScroll.on('call', function(func, way, obj) {
+  if (func === 'counter' && way === 'enter') {
+      $('.counter').counterUp({
+          delay: 1000,
+          time: 2000
+      });
+  }
+});
+
+
+
+
+
